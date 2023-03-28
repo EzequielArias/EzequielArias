bcaadb1884ec98f135839295deb0da259881494a
EzequielArias <ezequielariasdev@gmail.com>
2023-03-27 23:19:41 -0300

    Navbar responsive

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useRef, useState } from 'react'
+import { FaBars, FaTimes } from "react-icons/fa";
+import './Navbar.css'
+
+const Navbar = () => {
+    const navRef = useRef()
+    const [btn,setBtn] = useState(false)
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav")
+        setBtn(!btn)
+    }
+
+  return (
+    <div className={btn ? 'no-line' : 'line'}>
+    <div className='Navbar-container_links' ref={navRef}>
+          <a>Home</a>
+          <a>Sobre mi</a>
+          <a>Proyectos</a>
+          <a>Contacto</a>
+    </div>
+    <button 
+          className='nav-btn'
+          onClick={showNavbar}>
+            {btn ?  <FaTimes/> : <FaBars/>}
+        </button>
+    </div>
+)
+}
+
+export default Navbar
